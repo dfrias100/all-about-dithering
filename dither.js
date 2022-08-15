9a71a4ec2405fde1b1f575101145f5ddce79ae24
Daniel Frias <dfrias41@gmail.com>
2022-08-14 20:49:07 -0500

    added customization of algorithm to the html & js

--- dither.js
+++ dither.js
@@ -1,9 +1,10 @@
 export function dither(event) {
-    var input_canvas = event.target.myParam;
+    var input_canvas = event.target.myParam.canvas;
+    var parameters = event.target.myParam;
     var input_context = input_canvas.getContext("2d");
     var input_data = input_context.getImageData(0, 0, input_canvas.width, input_canvas.height);
 
-    input_data = floyd_steinberg(input_data, input_canvas.width, input_canvas.height);
+    input_data = floyd_steinberg(input_data, input_canvas.width, input_canvas.height, parameters);
 
     input_context.putImageData(input_data, 0, 0);
 }
@@ -34,29 +35,41 @@ function color_clamp(color) {
     return color;
 }
 
-function quantize(color) {
+function quantize(color, num_shades) {
     var quantized_color = { red: 0, green: 0, blue: 0, alpha: color.alpha };
-    quantized_color.red = Math.round(color.red / 255) * 255;
-    quantized_color.green = Math.round(color.green / 255) * 255;
-    quantized_color.blue = Math.round(color.blue / 255) * 255;
+    var factor = 255 / (num_shades - 1);
+    quantized_color.red = Math.round(color.red / factor) * factor;
+    quantized_color.green = Math.round(color.green / factor) * factor;
+    quantized_color.blue = Math.round(color.blue / factor) * factor;
     return quantized_color;
 }
 
-function floyd_steinberg(image_data_object, width, height) {
+function floyd_steinberg(image_data_object, width, height, other_parameters) {
+    // Define constants for the error diffusion
     var seven_sixteenths = 7.0 / 16.0;
     var three_sixteenths = 3.0 / 16.0;
     var one_sixteenth    = 1.0 / 16.0;
     var five_sixteenths  = 5.0 / 16.0;
 
+    // We will make the array 3D to make the code easier to read
     var multi_array_data = [];
+
+    // Splice() is only available on a normal array (ImageData stores a Uint8ClampedArray)
     var image_data_as_array = Array.prototype.slice.call(image_data_object.data);
     
-    image_data_as_array = make_gray_scale(image_data_as_array, width, height);
+    // Make the image gray scale
+    if (other_parameters.grayscale) {
+        image_data_as_array = make_gray_scale(image_data_as_array, width, height);
+    }
 
+    // Push all the rows into the new array
     while (image_data_as_array.length > 0) {
         multi_array_data.push(image_data_as_array.splice(0, width * 4));
     }
 
+    // For every row, and for every 4 values, create a color structure and push
+    // the color structure into the temporary array, then store the newly formed row 
+    // in the multi_array_data array.
     for (var i = 0; i < height; i++) {
         var row = [];
 
@@ -75,49 +88,45 @@ function floyd_steinberg(image_data_object, width, height) {
         multi_array_data[i] = row;
     }
 
+    // Floyd-Steinberg dithering algorithm
     for (var j = 0; j < height; j++) {
         for (var i = 0; i < width; i++) {
             var old_color = multi_array_data[j][i];
-            var new_color = quantize(old_color);
+            var new_color = quantize(old_color, other_parameters.num_shades);
 
             multi_array_data[j][i] = new_color;
 
-            var error = { red: old_color.red - new_color.red, green: old_color.green - new_color.green, blue: old_color.blue - new_color.blue };
+            var error = { 
+                red: old_color.red - new_color.red, 
+                green: old_color.green - new_color.green, 
+                blue: old_color.blue - new_color.blue 
+            };
 
+            // Ignore diffusion if the pixel is on the edge of the image
             if ((i + 1) < width) {
-                multi_array_data[j][i + 1].red += error.red * seven_sixteenths;
-                multi_array_data[j][i + 1].green += error.green * seven_sixteenths;
-                multi_array_data[j][i + 1].blue += error.blue * seven_sixteenths;
-
+                multi_array_data[j][i + 1] = addError(multi_array_data[j][i + 1], error, seven_sixteenths);
                 multi_array_data[j][i + 1] = color_clamp(multi_array_data[j][i + 1]);
 
                 if ((j + 1) < height) {
-                    multi_array_data[j + 1][i + 1].red += error.red * one_sixteenth;
-                    multi_array_data[j + 1][i + 1].green += error.green * one_sixteenth;
-                    multi_array_data[j + 1][i + 1].blue += error.blue * one_sixteenth;
-
+                    multi_array_data[j + 1][i + 1] = addError(multi_array_data[j + 1][i + 1], error, one_sixteenth);
                     multi_array_data[j + 1][i + 1] = color_clamp(multi_array_data[j + 1][i + 1]);
                 }
             }
 
+            // Same reasoning as above
             if ((j + 1) < height) {
                 if ((i - 1) >= 0) {
-                    multi_array_data[j + 1][i - 1].red += error.red * three_sixteenths;
-                    multi_array_data[j + 1][i - 1].green += error.green * three_sixteenths;
-                    multi_array_data[j + 1][i - 1].blue += error.blue * three_sixteenths;
-
+                    multi_array_data[j + 1][i - 1] = addError(multi_array_data[j + 1][i - 1], error, three_sixteenths);
                     multi_array_data[j + 1][i - 1] = color_clamp(multi_array_data[j + 1][i - 1]);
                 }
 
-                multi_array_data[j + 1][i].red += error.red * five_sixteenths;
-                multi_array_data[j + 1][i].green += error.green * five_sixteenths;
-                multi_array_data[j + 1][i].blue += error.blue * five_sixteenths;
-
+                multi_array_data[j + 1][i] = addError(multi_array_data[j + 1][i], error, five_sixteenths);
                 multi_array_data[j + 1][i] = color_clamp(multi_array_data[j + 1][i]);
             }
         }
     }
 
+    // When finished, place the data back into the image data object and return it
     for (var i = 0; i < width; i++) {
         for (var j = 0; j < height; j++) {
             var color = multi_array_data[j][i];
@@ -129,6 +138,13 @@ function floyd_steinberg(image_data_object, width, height) {
     }
 
     return image_data_object;
+}
+
+function addError(color, error, coefficient) {
+    color.red += error.red * coefficient;
+    color.green += error.green * coefficient;
+    color.blue += error.blue * coefficient;
+    return color;
 }
 
 function getColor(x, y, width, image_data) {
--- main.js
+++ main.js
@@ -1,6 +1,33 @@
 import { dither } from "./dither.js";
 
-var image_canvas;
+let input_file = document.getElementById("image_file");
+input_file.addEventListener("change", load_image, false);
+
+let input_shades = document.getElementById("num_shades");
+input_shades.addEventListener("change", number_change, false);
+
+let input_grayscale = document.getElementById("grayscale_check");
+input_grayscale.addEventListener("change", checkbox_change, false);
+
+var user_image;
+var image_canvas = document.getElementById("dithered_image");
+var dither_button = document.getElementById("dither_button");
+dither_button.myParam = { canvas: null, grayscale: false, num_shades: 2 };
+
+function number_change(event) {
+    dither_button.myParam.num_shades = event.target.value;
+}
+
+function checkbox_change(event) {
+    dither_button.myParam.grayscale = event.target.checked;
+}
+
+function before_dither(event) {
+    image_canvas.getContext("2d").clearRect(0, 0, image_canvas.width, image_canvas.height);
+    image_canvas.getContext("2d").drawImage(user_image, 0, 0);
+    dither_button.myParam.canvas = image_canvas;
+    dither(event);
+}
 
 function load_image(event) {
     console.log(event);
@@ -13,23 +40,17 @@ function load_image(event) {
     reader.onload = function(event) {
         img_tag.src = event.target.result;
 
-        var user_image = new Image();
+        user_image = new Image();
         user_image.src = event.target.result;
 
         user_image.onload = function() {
-            image_canvas = document.getElementById("dithered_image");
             image_canvas.width = user_image.width;
             image_canvas.height = user_image.height;
             image_canvas.getContext("2d").drawImage(user_image, 0, 0);
-            
-            let dither_button = document.getElementById("dither_button");
-            dither_button.addEventListener("click", dither, false);
-            dither_button.myParam = image_canvas;
         }
+
+        dither_button.addEventListener("click", before_dither, false);
     }
 
     reader.readAsDataURL(selected_file);
 }
-
-let input = document.getElementById("image_file");
-input.addEventListener("change", load_image, false);
